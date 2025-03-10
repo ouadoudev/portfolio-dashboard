deca56bc0b06185f241949b67ca2432687a52c4f
Mohamed Ouadou <uoaduomohamed@gmail.com>
2025-03-10 01:55:44 +0000

    Define User schema with fields for profile information, skills, and work status

---
+++ lib/models/User.ts
@@ -0,0 +1,34 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+    {
+        id: {type: Number, required: true},
+        fullName: {type: String, required: true},
+        image: {type: String, required: true},
+        title: {type: String, required: true},
+        tagline: {type: String, required: true},
+        introduction:{type: String, required:true},
+        KeySkills: [{type: String}],
+        status: {
+            type: String,
+            enum: [
+              "Seeking New Career Opportunities",
+              "Open to Work",
+              "Freelancing",
+              "Employed",
+              "Available for Collaboration",
+              "Working on Personal Projects",
+              "Interning",
+              "Exploring New Technologies",
+              "Unavailable",
+              "Remote Only",
+              "Contract-Based Work Only",
+            ],
+            default: "Open to Work",
+          },
+          cv: {type: String, required: true},
+          yearsOfExperience: {type: Number, default: 1},
+          
+    }
+)
+export default models.User || model('User', UserSchema);
