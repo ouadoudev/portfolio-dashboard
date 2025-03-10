ee9e774749059e572db318479b097cd97b504733
Mohamed Ouadou <uoaduomohamed@gmail.com>
2025-03-10 01:40:26 +0000

    Set up Mongoose with type definitions and establish database connection in db.ts

---
+++ lib/db.ts
@@ -0,0 +1,37 @@
+import mongoose, { Connection } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+declare global {
+  var mongoose: { conn: Connection | null, promise: Promise<Connection> | null };
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongooseInstance) => {
+      return mongooseInstance.connection;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectToDatabase;
